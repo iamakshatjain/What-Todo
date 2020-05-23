ab9438a635baf4dc5fa13a4f581b95e4aa21a863
iamakshatjain <jain99akshat@gmail.com>
2020-05-23 16:44:37 +0530

    Updates

---
+++ precache-manifest.766c8aaacaf22b33524c7b5248e64fe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4788c37a5120ba54d571e965f3f01a98",
+    "url": "/What-Todo/index.html"
+  },
+  {
+    "revision": "cc32dc7891122fa72776",
+    "url": "/What-Todo/static/css/main.37aaf073.chunk.css"
+  },
+  {
+    "revision": "8a4a23a685f6f1ca1154",
+    "url": "/What-Todo/static/js/2.1ba72eab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/What-Todo/static/js/2.1ba72eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc32dc7891122fa72776",
+    "url": "/What-Todo/static/js/main.e9cb749b.chunk.js"
+  },
+  {
+    "revision": "a0165bf4152b05a3406c",
+    "url": "/What-Todo/static/js/runtime-main.74267fe3.js"
+  },
+  {
+    "revision": "78fd7a96f8fd057ab3465424fc51f918",
+    "url": "/What-Todo/static/media/nothing.78fd7a96.svg"
+  }
+]);
